19ee9756de0b7b8b889e0cde834d9ff80bf723f8
Jake Gonchar <jake098877@gmail.com>
2020-04-04 19:36:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
 var  obj = { prop: 1 }
-delete obj[key]
-return obj
+var newObj = delete obj[key]
+return newObj
 }
