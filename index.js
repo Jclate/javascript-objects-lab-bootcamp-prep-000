d86d390e03071fdec77c14607d198dad6b556c5b
Jake Gonchar <jake098877@gmail.com>
2020-04-04 19:02:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ return obj
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var obj = { prop: 1 }
+  const obj = { prop: 1 }
   obj = { [key]: value }
   return obj
 }
