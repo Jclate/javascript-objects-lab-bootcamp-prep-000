43cf494c20ea937af0fd0db1e37a53f6d20fb8b4
Jake Gonchar <jake098877@gmail.com>
2020-04-04 18:45:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function updateObjectWithKeyAndValue(key, value) {
   console.log(key)
   var value = value
   console.log (value)
-  var obj = Object.assign({}, { prop: 1 }, { key: value })
+  var obj = Object.assign({}, { prop: 1 }, { key: 'value' })
   return obj
 
 }
-updateObjectWithKeyAndValue('prop2', 2)
+//updateObjectWithKeyAndValue('prop2', 2)
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 var obj = object
 obj.key = 'value'
